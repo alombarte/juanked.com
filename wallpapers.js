1fd9157d6b1711a8c5fe769ad21c707de9fd52a8
Albert Lombarte <alombarte@zinio.com>
2016-09-02 09:52:22 +0200

    Initial version of juanked. PRs are welcome

---
+++ wallpapers.js
@@ -0,0 +1,42 @@
+images = [
+  {
+    "title": "Nyan Cat",
+    "url": "https://media.giphy.com/media/l6fzXjHwAWXCM/giphy.gif"
+  },
+  { 
+    "title": "Dear Carmen",
+    "url": "https://i.ytimg.com/vi/VW0k_NiEM6U/maxresdefault.jpg"
+  },
+  {
+    "title": "El Fary",
+    "url": "http://spf.fotolog.com/photo/47/1/80/jartokan/1098803800_f.jpg"
+  },
+  {
+    "title": "David Hasselhof",
+    "url": "http://static2.comicvine.com/uploads/screen_kubrick/0/40/749137-hoff.jpg"
+  },
+  {
+    "title": "Salchiiiipaaaaaapaaaa",
+    "url": "https://i.ytimg.com/vi/FuJh1gDL76s/maxresdefault.jpg"
+  },
+  {
+    "title": "Leticia, police",
+    "url": "http://quemedices.diezminutos.es/var/qmd/storage/images/noticias_famosos/leticia_sabater_saca_nuevo_disco/sabater01/995400-1-esl-ES/sabater01_ampliacion.jpg"
+  },
+  {
+    "title": "Leticia, Marbella",
+    "url": "http://media.gettyimages.com/photos/leticia-sabater-is-seen-on-july-6-2015-in-marbella-spain-picture-id480906184"
+  },
+  {
+    "title": "Pollo frito",
+    "url": "https://i.ytimg.com/vi/7x-Y0yvRwpc/maxresdefault.jpg"
+  },
+  {
+    "title": "Banana (Fruity flash)",
+    "url": "http://www.cgsociety.org/cgsarchive/newgallerycrits/g06/29806/29806_1236311607_large.jpg"
+  },
+  {
+    "title": "Eat shit",
+    "url": "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/web05/2011/12/30/15/enhanced-buzz-wide-32333-1325276446-51.jpg"
+  }
+];
