e616a6d7b87b31179b2efe2942eac7193b46c6b2
A. Lombarte <alombarte@users.noreply.github.com>
2021-09-16 11:09:49 +0200

    Remove broken images

--- wallpapers.js
+++ wallpapers.js
@@ -3,10 +3,6 @@ images = [
     "title": "Nyan Cat",
     "url": "https://media.giphy.com/media/l6fzXjHwAWXCM/giphy.gif"
   },
-  { 
-    "title": "Dear Carmen",
-    "url": "https://i.ytimg.com/vi/VW0k_NiEM6U/maxresdefault.jpg"
-  },
   {
     "title": "El Fary",
     "url": "http://photos1.blogger.com/blogger/2833/1237/1600/fary1.jpg"
@@ -20,23 +16,11 @@ images = [
     "url": "https://i.ytimg.com/vi/FuJh1gDL76s/maxresdefault.jpg"
   },
   {
-    "title": "Leticia, police",
-    "url": "http://quemedices.diezminutos.es/var/qmd/storage/images/noticias_famosos/leticia_sabater_saca_nuevo_disco/sabater01/995400-1-esl-ES/sabater01_ampliacion.jpg"
-  },
-  {
     "title": "Leticia, Marbella",
     "url": "http://media.gettyimages.com/photos/leticia-sabater-is-seen-on-july-6-2015-in-marbella-spain-picture-id480906184"
   },
   {
     "title": "Pollo frito",
     "url": "https://i.ytimg.com/vi/7x-Y0yvRwpc/maxresdefault.jpg"
-  },
-  {
-    "title": "Banana (Fruity flash)",
-    "url": "http://www.cgsociety.org/cgsarchive/newgallerycrits/g06/29806/29806_1236311607_large.jpg"
-  },
-  {
-    "title": "Eat shit",
-    "url": "https://img.buzzfeed.com/buzzfeed-static/static/enhanced/web05/2011/12/30/15/enhanced-buzz-wide-32333-1325276446-51.jpg"
   }
 ];
